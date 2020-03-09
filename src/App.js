ab9d4aaa2ff4c06aaa1e8d343ae8d092e6a3d632
raomaiping <2582395486@qq.com>
2020-03-09 20:59:06 +0800

    JSX

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React,{Component} from 'react'
 class App extends Component{
     render(){
         return(
-            <div>
-                Hello word
-            </div>
+            <ul className="my-list">
+                <li>{false ? 'http://raomaiping.xyz' : '前端小菜鸟吖'}</li>
+                <li>I Love React</li>
+            </ul>
         )
+
+        // var child1 = React.createElement('li',null,'http://raomaiping.xyz')
+        // var child2 = React.createElement('li',null,'I Love React')
+        // var root = React.createElement('ul',{className:'my-list'},child1,child2)
     }
 }
 
